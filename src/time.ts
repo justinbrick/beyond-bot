d16b4abb8e71dc7458d37a17ab8493dfa3313103
justinbrick <justinbrick1@gmail.com>
2022-07-04 12:28:46 -0400

    Fix times

--- src/time.ts
+++ src/time.ts
@@ -2,5 +2,5 @@ import { DateTime } from 'luxon';
 
 // I put this here so we can easily simulate different times
 
-//export const getCurrentTime = () => DateTime.now().plus({ days: 7 });
-export const getCurrentTime = () => DateTime.fromISO('2022-10-05');
+export const getCurrentTime = () => DateTime.now();
+//export const getCurrentTime = () => DateTime.fromISO('2022-10-05');
